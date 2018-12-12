07e00edce7fa64900b4ae321dbfbd7d8a9731701
bundai223 <bundai223@gmail.com>
2018-12-12 13:00:26 +0900

    margin削除

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <main>
-      <router-view></router-view>
-    </main>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -24,27 +22,4 @@ body {
   color: #2c3e50;
 }
 
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
 </style>
